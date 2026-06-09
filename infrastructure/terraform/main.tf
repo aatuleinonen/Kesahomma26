003dd22c567583b22edf33b5974403f8f68adaba
Juha Leinonen <juha.leinonen@nexigroup.com>
2026-06-09 14:51:56 +0300

    Fix pipeline Terraform file paths and CodeBuild policy (#44)

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -5,17 +5,18 @@ data "aws_caller_identity" "current" {}
 
 # Bounding logs_retention_time to 7 days fulfills the MVP log retention requirement (Issue #11)
 module "aws_infra_pipeline" {
-source = "git::https://github.com/Nets-Platform-Enablement/tf-module-aws-infra-pipeline.git?ref=c97f3f5d455ebd30edea9c37116f5c1f370e5f2c"
+  source = "git::https://github.com/Nets-Platform-Enablement/tf-module-aws-infra-pipeline.git?ref=c97f3f5d455ebd30edea9c37116f5c1f370e5f2c"
 
-environment          = "dev"
-branch_name          = "main"
-github_repository_id = "aatuleinonen/kesahomma26"
-variables_file       = "dev.tfvars"
-tfbackend_file       = "dev.s3.tfbackend"
-directory            = "infrastructure/terraform"
+  environment          = "dev"
+  branch_name          = "main"
+  github_repository_id = "aatuleinonen/kesahomma26"
+  variables_file       = "./dev.tfvars"
+  tfbackend_file       = "./dev.s3.tfbackend"
+  directory            = "infrastructure/terraform"
 
-enable_checkov       = true
-require_checkov_pass = false
-logs_retention_time  = 7
-emails               = ["aatu.leinonen@gmail.com", "juha.leinonen@gmail.com"]
+  enable_checkov       = true
+  require_checkov_pass = false
+  logs_retention_time  = 7
+  emails               = ["aatu.leinonen@gmail.com", "juha.leinonen@gmail.com"]
+  managed_policies     = ["PowerUserAccess"]
 }
